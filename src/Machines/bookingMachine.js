ab56d62cb54ff89a43f811e4fa8f8362f947d76a
Natalia Fernandez <naicolsx@gmail.com>
2023-03-27 12:36:17 -0500

    XState, Maquina de estados, eventos y transiciones, acciones

---
+++ src/Machines/bookingMachine.js
@@ -0,0 +1,46 @@
+import { createMachine } from "xstate";
+
+const bookingMachine = createMachine(
+  {
+    id: "buy plane tickets",
+    initial: "initial",
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: "search",
+            actions: "imprimirInicio",
+          },
+        },
+      },
+      search: {
+        entry: 'imprimirEntrada',
+        exit: 'imprimirSalida',
+        on: {
+          CONTINUE: "passengers",
+          CANCEL: "initial",
+        },
+      },
+      tickets: {
+        on: {
+          FINISH: "initial",
+        },
+      },
+      passengers: {
+        on: {
+          DONE: "tickets",
+          CANCEL: "initial",
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      imprimirInicio: () => console.log("Imprimir inicio"),
+      imprimirEntrada: () => console.log('Imprimir entrada search'),
+      imprimirSalida: () => console.log('Imprimir salida search'),
+    },
+  }
+);
+
+export default bookingMachine;
